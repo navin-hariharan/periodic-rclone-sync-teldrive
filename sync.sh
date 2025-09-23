37ca6726268579f4ddbbc9fa2accbaa7d74f002c
navin-hariharan <pepperjack1210@gmail.com>
2025-09-23 18:17:11 +0100

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -2,7 +2,7 @@
 
 if [ "$(($(date +%s) / 60 % ${INTERVAL}))" -eq "0" ]; then
   echo "==== $(date) Starting rclone sync ===="
-  rclone sync "${SOURCE}" "${TARGET}" ${OPTIONS}
+  rclone "${MODE}" "${SOURCE}" "${TARGET}" ${OPTIONS}
   echo "periodic-rclone-sync complete: ${SOURCE} → ${TARGET}"
   echo "==== $(date) Sync finished ===="
 fi
